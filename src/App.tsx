6db381ad9fb7d5ff4a9267effcfe06003574f697
unknown <ainimwo@yandex.ru>
2026-03-17 16:57:43 +0300

    click 1000 -> 10

--- src/App.tsx
+++ src/App.tsx
@@ -63,7 +63,7 @@ export default function App() {
                         CLICK!
                     </button>
 
-                    {counter >= 1000 && (
+                    {counter >= 10 && (
                         <button
                             style={{
                                 width: '100%',
